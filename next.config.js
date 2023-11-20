42fd3099f8b5166e14c94272343963d25dc7a21a
sydwell <mohamed.souidi.dev@gmail.com>
2023-11-20 20:41:48 +0100

    Custom the next configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      dangerouslyAllowSVG: true,
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'tailwindui.com',
+          port: '',
+          pathname: '/img/**',
+        },
+      ],
+    },
+    webpack: (config) => {
+      config.externals.push('pino-pretty', 'lokijs', 'encoding');
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
